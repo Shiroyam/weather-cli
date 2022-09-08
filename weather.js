76a3315a68a110dbb63c7aef6993dd3b65b23eff
Semyon Zasov <semenmaslina@gmail.com>
2022-09-08 19:23:01 +0300

    add(services): storage

--- weather.js
+++ weather.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
-import {getArgs} from "./helpers/args.js";
-import {printHelp} from "./services/log.js";
+import { getArgs } from "./helpers/args.js";
+import { printError, printHelp, printSuccess } from "./services/log.js";
+import { saveKeyValue } from "./services/storage.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCli = () => {
-    const args = getArgs(process.argv);
-    console.log(args);
-    if (args.h) {
-        printHelp();
-    }
-    if (args.s) {
-        //вывод город
-    }
-    if (args.t) {
-        //сохранить токен
-    }
+  const args = getArgs(process.argv);
+  console.log(args);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+    //вывод город
+  }
+  if (args.t) {
+    return saveToken(args.t);
+  }
 };
 
 initCli();
